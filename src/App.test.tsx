d08e8055228c91ee0aea128b35e631852fefaf49
vasu.senawongse <vasu@coursesquare.co>
2021-06-27 22:36:41 +0700

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,23 +5,29 @@ import CalSumAssured from './CalSumAssured';
 import CalPremium from './CalPremium';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-const store = configureStore();
-test('Should renders the Calc SA', () => {
-  const component = shallow(<App />);
-  expect(component.contains(<CalSumAssured />)).toBe(true);
-});
+describe('App', () => {
+  let store: any;
+  beforeEach(() => {
+    store = configureStore();
+  });
 
-test('Should renders the Calc Premium', () => {
-  const component = shallow(<App />);
-  expect(component.contains(<CalPremium />)).toBe(true);
-});
+  test('Should renders the Calc SA', () => {
+    const component = shallow(<App />);
+    expect(component.contains(<CalSumAssured />)).toBe(true);
+  });
 
-test('Should renders Calc SA Tab', () => {
-  const component = shallow(<App />);
-  expect(component.find('#tab1').exists()).toBe(true);
-});
+  test('Should renders the Calc Premium', () => {
+    const component = shallow(<App />);
+    expect(component.contains(<CalPremium />)).toBe(true);
+  });
 
-test('Should renders Calc Premium Tab', () => {
-  const component = shallow(<App />);
-  expect(component.find('#tab2').exists()).toBe(true);
+  test('Should renders Calc SA Tab', () => {
+    const component = shallow(<App />);
+    expect(component.find('#tab1').exists()).toBe(true);
+  });
+
+  test('Should renders Calc Premium Tab', () => {
+    const component = shallow(<App />);
+    expect(component.find('#tab2').exists()).toBe(true);
+  });
 });
